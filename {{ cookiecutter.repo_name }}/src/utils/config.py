f6faad8f493248b56c7560e8e22e2be90b8ce186
gregoire hornung <greghor4@gmail.com>
2020-07-22 12:05:43 +0200

    fix paths

--- {{ cookiecutter.repo_name }}/src/utils/config.py
+++ {{ cookiecutter.repo_name }}/src/utils/config.py
@@ -15,10 +15,10 @@ SRC_DIR = PROJECT_DIR + "src/"
 CODE_ASSETS_DIR = SRC_DIR + "assets/"
 
 DATA_DIR = PROJECT_DIR + "data/"
-RAW_DATA_DIR = DATA_DIR + f"raw//"
+RAW_DATA_DIR = DATA_DIR + f"raw/"
 
-PROCESSED_DATA_DIR = DATA_DIR + f"processed//"
-SAMPLE_DATA_DIR = DATA_DIR + f"sample//"
+PROCESSED_DATA_DIR = DATA_DIR + f"processed/"
+SAMPLE_DATA_DIR = DATA_DIR + f"sample/"
 INTERIM_DIR = DATA_DIR + f"interim/"
 ASSETS_DATA_DIR = DATA_DIR + f"assets/"
 
